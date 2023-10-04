62dc15b322b5d2754d49d382006c314332704852
Thurneer <florian00m14@gmail.com>
2023-10-04 23:45:04 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,59 +8,56 @@ import Client from "@/models/client-model";
 import { useReservationContext } from "@/store/reservation-context";
 
 const BookingApp: NextPage = () => {
-  const clientCtx = useClientContext();
-  const reservationCtx = useReservationContext();
-  const test = () => {
-    // Egyedi ügyfelek tárolására szolgáló objektum létrehozása
-    const uniqueClientsMap: { [name: string]: Client } = {};
+  // const clientCtx = useClientContext();
+  // const reservationCtx = useReservationContext();
+  // const test = () => {
+  //   // Egyedi ügyfelek tárolására szolgáló objektum létrehozása
+  //   const uniqueClientsMap: { [name: string]: Client } = {};
 
-    // Az eredeti tömb bejárása és egyedi ügyfelek hozzáadása az objektumhoz
-    clientCtx.clients.forEach((client) => {
-      if (!uniqueClientsMap[client.name]) {
-        uniqueClientsMap[client.name] = client;
-      }
-    });
+  //   // Az eredeti tömb bejárása és egyedi ügyfelek hozzáadása az objektumhoz
+  //   clientCtx.clients.forEach((client) => {
+  //     if (!uniqueClientsMap[client.name]) {
+  //       uniqueClientsMap[client.name] = client;
+  //     }
+  //   });
 
-    // Az egyedi ügyfeleket tartalmazó tömb létrehozása
-    const uniqueClients: Client[] = Object.values(uniqueClientsMap);
+  //   // Az egyedi ügyfeleket tartalmazó tömb létrehozása
+  //   const uniqueClients: Client[] = Object.values(uniqueClientsMap);
 
-    uniqueClients.sort((a, b) => a.name.localeCompare(b.name));
-    console.log(uniqueClients);
+  //   uniqueClients.sort((a, b) => a.name.localeCompare(b.name));
+  //   console.log(uniqueClients);
 
-    // Azoknak az ügyfeleknek a gyűjtése, amelyeknek nincs ID-jük az egyedi ügyfelek tömbjében
-    const missingIds: string[] = clientCtx.clients.reduce<string[]>((acc, client) => {
-      if (!uniqueClientsMap[client.name] || uniqueClientsMap[client.name].id !== client.id) {
-        acc.push(client.id);
-      }
-      return acc;
-    }, []);
-    console.log(missingIds);
+  //   // Azoknak az ügyfeleknek a gyűjtése, amelyeknek nincs ID-jük az egyedi ügyfelek tömbjében
+  //   const missingIds: string[] = clientCtx.clients.reduce<string[]>((acc, client) => {
+  //     if (!uniqueClientsMap[client.name] || uniqueClientsMap[client.name].id !== client.id) {
+  //       acc.push(client.id);
+  //     }
+  //     return acc;
+  //   }, []);
+  //   console.log(missingIds);
 
-    reservationCtx.reservations.forEach((reservation) => {
-      if (reservation.clientId && missingIds.includes(reservation.clientId)) {
-        const clientName = clientCtx.getClientById(reservation.clientId)?.name;
-        const keptClient = uniqueClients.find((client) => client.name === clientName);
-        if (keptClient) {
-          const updatedReservation = {
-            ...reservation,
-            clientId: keptClient.id,
-          }
-          reservationCtx.updateReservation(reservation.id, updatedReservation);
-        }
-      }
-    });
+  //   reservationCtx.reservations.forEach((reservation) => {
+  //     if (reservation.clientId && missingIds.includes(reservation.clientId)) {
+  //       const clientName = clientCtx.getClientById(reservation.clientId)?.name;
+  //       const keptClient = uniqueClients.find((client) => client.name === clientName);
+  //       if (keptClient) {
+  //         const updatedReservation = {
+  //           ...reservation,
+  //           clientId: keptClient.id,
+  //         }
+  //         reservationCtx.updateReservation(reservation.id, updatedReservation);
+  //       }
+  //     }
+  //   });
 
-    missingIds.forEach((id) => {
-      clientCtx.removeClient(id);
-    });
-  };
+  //   missingIds.forEach((id) => {
+  //     clientCtx.removeClient(id);
+  //   });
+  // };
   return (
     <>
       <PageHead />
       <Calendar />
-      <Box sx={{ paddingTop: 5 }}>
-        <Button onClick={test}>Teszt</Button>
-      </Box>
     </>
   );
 };
